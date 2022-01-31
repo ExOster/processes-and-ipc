f086ab541843e973a8d9d2110ed51e4c6b27e11b
ExOster <Oscartensta@gmail.com>
2022-01-31 15:31:47 +0100

    Our work so far

--- mandatory/src/pipeline.c
+++ mandatory/src/pipeline.c
@@ -7,20 +7,87 @@
 #define WRITE 1
 
 void child_a(int fd[]) {
-
-  // TODO: Add code here.
-
+  printf(" CHILD A <%ld> I'm alive! My PID is <%ld> and my parent got PID <%ld>.\n",
+         (long) getpid(), (long) getpid(), (long) getppid());
+        
+        // Close the pipe read descriptor.
+        close(fd[READ]);
+        // Redirect STDOUT to write to the pipe.
+        dup2(fd[WRITE], STDOUT_FILENO);
+        // Close the dangling pipe write descriptor.
+        close(fd[WRITE]);
+        // Execute ls -l
+        //execlp("ls", "ls", NULL);
+  puts("hej");
+  printf(" CHILD  A <%ld> Goodbye!\n",
+         (long) getpid());
+  exit(EXIT_SUCCESS);
 }
 
 void child_b(int fd[]) {
+   printf(" CHILD B <%ld> I'm alive! My PID is <%ld> and my parent got PID <%ld>.\n",
+         (long) getpid(), (long) getpid(), (long) getppid());
+    
+    // The child process will execute wc.
+    // Close the pipe write descriptor.
+    close(fd[WRITE]);
+    // Redirect STDIN to read from the pipe.
+    dup2(fd[READ], STDIN_FILENO);
+    // Close the dangling pipe read descriptor.
+    //read(fd[READ],"aaaaaaaa",1);
+    
+    close(fd[READ]);
+    // Execute wc
+    //execlp("wc", "wc", NULL);  
 
-  // TODO: Add code here.
-
+  printf(" CHILD B <%ld> Goodbye!\n",
+         (long) getpid());
+  exit(EXIT_SUCCESS);
 }
+
+void parent(int pidIDs[]) {
+  printf("PARENT <%ld> My PID is <%ld> and I spawned a child with PID <%ld>.\n",
+         (long) getpid(), (long) getpid(), (long) pidIDs[0]);
+  printf("PARENT <%ld> My PID is <%ld> and I spawned a child with PID <%ld>.\n",
+         (long) getpid(), (long) getpid(), (long) pidIDs[1]);
+  wait(NULL);
+  wait(NULL);
+
+  execlp("ls", "ls", NULL);
+
+  printf("PARENT <%ld> Goodbye!\n",
+         (long) getpid());
+  exit(EXIT_SUCCESS);
+}
+
+
+
 
 int main(void) {
   int fd[2];
+  pid_t c1, c2, par;
+  int pidIDs[2];
+  
+  // if (pipe(fd) == -1) {
+  //   perror("Creating pipe");
+  //   exit(EXIT_FAILURE);
+  // }
+  pipe(fd);
+  c1 = fork();
+  pidIDs[0]=c1;
+  if (c1 == 0) {
+    child_a(fd);
 
-  // TODO: Add code here.
+  } else {
+      c2 = fork();
+      pidIDs[1]=c2;
+      
+      if (c2 == 0) {
+        child_b(fd);
+        
+      } else {
+        parent(pidIDs);
+      }
+  }
 
 }
--- mandatory/src/signals.c
+++ mandatory/src/signals.c
@@ -1,56 +1,93 @@
-#include <stdio.h>   // puts(), printf()
-#include <signal.h>  // SIGFPE, SIGSEGV, SIGINT
-#include <stdlib.h>  // exit(), EXIT_SUCCESS, EXIT_FAIURE
-#include <unistd.h>  // getpid(), pause()
-#include <stdbool.h> // true, false
+#include <stdio.h>    // puts(), printf(), perror(), getchar()
+#include <stdlib.h>   // exit(), EXIT_SUCCESS, EXIT_FAILURE
+#include <unistd.h>   // getpid(), getppid(),fork()
+#include <sys/wait.h> // wait()
 
-bool done = false;
+#define READ  0
+#define WRITE 1
 
-int divide_by_zero() {
-  int a = 1;
-  int b = 0;
-  return a / b;
+void child_a(int fd[]) {
+  printf(" CHILD A <%ld> I'm alive! My PID is <%ld> and my parent got PID <%ld>.\n",
+         (long) getpid(), (long) getpid(), (long) getppid());
+        
+        // Close the pipe read descriptor.
+        close(fd[READ]);
+        // Redirect STDOUT to write to the pipe.
+        dup2(fd[WRITE], STDOUT_FILENO);
+        // Close the dangling pipe write descriptor.
+        close(fd[WRITE]);
+        // Execute ls -l
+        //execlp("ls", "ls", NULL);
+  puts("hej");
+  printf(" CHILD  A <%ld> Goodbye!\n",
+         (long) getpid());
+  exit(EXIT_SUCCESS);
 }
 
-void segfault() {
-  int *ptr = NULL;
-  *ptr = 42;
+void child_b(int fd[]) {
+   printf(" CHILD B <%ld> I'm alive! My PID is <%ld> and my parent got PID <%ld>.\n",
+         (long) getpid(), (long) getpid(), (long) getppid());
+    
+    // The child process will execute wc.
+    // Close the pipe write descriptor.
+    close(fd[WRITE]);
+    // Redirect STDIN to read from the pipe.
+    dup2(fd[READ], STDIN_FILENO);
+    // Close the dangling pipe read descriptor.
+    //read(fd[READ],"aaaaaaaa",1);
+    
+    close(fd[READ]);
+    // Execute wc
+    //execlp("wc", "wc", NULL);  
+
+  printf(" CHILD B <%ld> Goodbye!\n",
+         (long) getpid());
+  exit(EXIT_SUCCESS);
 }
 
-void signal_handler(int s) {
-  switch(s) {
-    case SIGFPE:
-      fputs("Caught SIGFPE: arithmetic exception, such as division by zero.\n", stderr);
-      exit(EXIT_FAILURE);
-    case SIGSEGV:
-      fputs("Caught SIGSEGV: segfault.\n", stderr);
-      exit(EXIT_FAILURE);
-      break;
-    case SIGINT:
-      fputs("Caught SIGINT: interactive attention signal, probably a ctrl+c.\n", stderr);
-      break;
-    case SIGUSR1:
-      puts("Hello!");
-      break;
-  }
+void parent(int pidIDs[]) {
+  printf("PARENT <%ld> My PID is <%ld> and I spawned a child with PID <%ld>.\n",
+         (long) getpid(), (long) getpid(), (long) pidIDs[0]);
+  printf("PARENT <%ld> My PID is <%ld> and I spawned a child with PID <%ld>.\n",
+         (long) getpid(), (long) getpid(), (long) pidIDs[1]);
+  wait(NULL);
+  wait(NULL);
+
+  execlp("ls", "ls", NULL);
+
+  printf("PARENT <%ld> Goodbye!\n",
+         (long) getpid());
+  exit(EXIT_SUCCESS);
 }
+
+
+
 
 int main(void) {
+  int fd[2];
+  pid_t c1, c2, par;
+  int pidIDs[2];
+  
+  // if (pipe(fd) == -1) {
+  //   perror("Creating pipe");
+  //   exit(EXIT_FAILURE);
+  // }
+  pipe(fd);
+  c1 = fork();
+  pidIDs[0]=c1;
+  if (c1 == 0) {
+    child_a(fd);
 
-  printf("My PID = %ld\n", (long) getpid());
+  } else {
+      c2 = fork();
+      pidIDs[1]=c2;
+      
+      if (c2 == 0) {
+        child_b(fd);
+        
+      } else {
+        parent(pidIDs);
+      }
+  }
 
-  // Install signal handlers.
-
-  // signal(SIGFPE,  signal_handler);
-
-  // divide_by_zero();
-  // segfault();
-
-  // Wait until a signal is delivered.
-
-  // pause();
-
-  puts("I'm done!");
-
-  exit(EXIT_SUCCESS);
 }
